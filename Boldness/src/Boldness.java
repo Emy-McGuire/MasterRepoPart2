35fc8152e1eb4ba2e98b1ffb4f6da13eaacaa235
lauren.sciullo <lauren.sciullo@MU-C1-06.mullen.pvt.k12.co.us>
2020-01-09 11:18:12 -0700

    Lauren created a method

--- Boldness/src/Boldness.java
+++ Boldness/src/Boldness.java
@@ -7,6 +7,13 @@ public class Boldness
 				System.out.println("Be BOLD!");
 				System.out.println("Right on!");
 				System.out.println("Never Stop!");
+				laurenMethod();
+				
 			}
+		
+		public static void laurenMethod()
+		{
+			System.out.println("Lauren trying to create a method now!");
+		}
 
 	}
